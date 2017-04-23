07fdf522a5bdb30ba61d9680fe4ee6533dd9aeca
ysiguman <victord675@gmail.com>
2017-04-23 21:04:56 +0200

    Vic's changes

--- main.cpp
+++ main.cpp
@@ -1,39 +1,8 @@
-/*
-________________________________________________________________________________________________________________________________________
-||____________________________________________________________________________________________________________________________________||
-||                                                                                                                                    ||
-||																																	  ||
-||							Budapesti Műszaki és Gazdaságtudományi Egyetem, 2016-2017, Spring Semester                                ||
-||										                                                                                              ||
-||										BASICS OF PROGRAMMING 2 : FINAL PROJECT														  ||
-||																																	  ||
-||																																	  ||
-||										BITMAP IMAGE TREATMENT USING C++         													  ||
-||																																      ||
-||																																	  ||
-||																										LAPORTE NATHAN CLAUDE         ||
-||																										Nept CODE : IBIS2E	          ||
-||																										Ver. 1.02A April 2017         ||
-||                                                                                                                                    ||
-||																																	  ||
-||																																	  ||
-||____________________________________________________________________________________________________________________________________||
-________________________________________________________________________________________________________________________________________
-*/
-
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
 #include "pixel.hpp"
-#pragma pack(push, 1)
 
 using namespace std;
 
 
-int pixel::get_Grey(const pixel p){
-	int grey = (p.rgb.red + p.rgb.green + p.rgb.blue) / 3;
-	return grey;
-}
 picture::picture(const char* filename){
     FILE* file;
     file = fopen(filename, "rb");
@@ -41,18 +10,17 @@ picture::picture(const char* filename){
     std::cout << sizeof(fileHeader) << std::endl;
 
     if(file != NULL) { // file opened
-        file_header(h);
-        size_t x = fread(&h, sizeof(fileHeader), 1, file); //reading the FILEHEADER
+        fread(&fileHeader, sizeof(fileHeader), 1, file); //reading the FILEHEADER
+        fread(&infoHeader, sizeof(infoHeader), 1, file); //reading the FILEHEADER
 
-        std::cout << x;
-        fread(&this->infoHeader, sizeof(infoHeader), 1, file);
-
+        std::cout << infoHeader.img_Width << std::endl;
         fclose(file);
     }
 }
 
 int main()
 {
+  new picture("lenaColor.bmp");
 	return 0;
 }
 
--- pixel.hpp
+++ pixel.hpp
@@ -1,92 +1,37 @@
-
-/*
-________________________________________________________________________________________________________________________________________
-||____________________________________________________________________________________________________________________________________||
-||                                                                                                                                    ||
-||																																	  ||
-||							Budapesti Műszaki és Gazdaságtudományi Egyetem, 2016-2017, Spring Semester                                ||
-||										                                                                                              ||
-||										BASICS OF PROGRAMMING 2 : FINAL PROJECT														  ||
-||																																	  ||
-||																																	  ||
-||										BITMAP IMAGE TREATMENT USING C++         													  ||
-||																																      ||
-||																																	  ||
-||																										LAPORTE NATHAN CLAUDE         ||
-||																										Nept CODE : IBIS2E	          ||
-||																										Ver. 1.02A April 2017         ||
-||                                                                                                                                    ||
-||																																	  ||
-||																																	  ||
-||____________________________________________________________________________________________________________________________________||
-________________________________________________________________________________________________________________________________________
-*/
-
 #include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-#pragma pack(push, 1)
+#pragma pack( 1)
+using namespace std;
 
-class pixel // each pixel is an object defined by :
+class picture //A Picture is a collection of pixels.
 {
-private:
+public:
+	struct // Its RGB values default pixels wil be black
+	{
+    unsigned char magicNumber[2]; //define the format type.
+    unsigned int  size; //Size of the image
+    int           reserved;
+    unsigned int  offset; //Starting point of the pixel matrix
+	}fileHeader ;
+  struct
+  {
+    int   info_Size;
+    int   img_Width;
+    int   img_Height;
+    short biPlanes;//Number of color planes
+    short bpp;
+    int   compression;
+    int   img_Size;
+    int   xppm;//X pixels per meter
+    int   yppm;//Y pixels per meter
+    int   color_Depth;
+    int   biClrImportant;//Number of important colors
+  } infoHeader;
 	struct // Its RGB values default pixels wil be black
 	{
 		int red = 0;
 		int green = 0;
 		int blue = 0;
 	}rgb;
-public:
-	pixel(); //Constructor
-	~pixel(); //Destructor
-	int get_Grey(const pixel p); //Calculating the grey value for Color -> Grey scale transform
-
-};
-
-class file_header
-{
-private:
-	struct{
-	unsigned char magicNumber[2]; //define the format type.
-	unsigned int size; //Size of the image
-	unsigned short int id_one; //First id
-	unsigned short int id_two; //second id
-	unsigned int offset; //Starting point of the pixel matrix
-};
-public:
-	file_header();
-	~file_header();
-};
-
-class info_header
-{
-private:
-	struct{
-	unsigned int   info_Size;
-    int            img_Width;
-    int            img_Height;
-    unsigned short biPlanes;//Number of color planes
-    unsigned short bpp;
-    unsigned int   compression;
-    unsigned int   img_Size;
-    int            xppm;//X pixels per meter
-    int            yppm;//Y pixels per meter
-    unsigned int   color_Depth;
-    unsigned int   biClrImportant;//Number of important colors
-	};
-public:
-	info_header();
-	~info_header();
-};
-
-
-class picture //A Picture is a collection of pixels.
-{
-private:
-	file_header fileHeader;
-	info_header infoHeader;
-	pixel matrix[0][0];
-public:
 	picture(const char* filename);
 	~picture();
 };
